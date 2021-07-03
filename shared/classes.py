464787744866df901a7419548cd2da8dbb4d160b
rendstrasser <rendstrasser@gmail.com>
2021-07-03 19:35:25 +0200

    added support for constraints and lagrangian to problem

---
+++ shared/classes.py
@@ -0,0 +1,131 @@
+import numpy as np
+from typing import Callable
+from dataclasses import dataclass
+
+@dataclass
+class MinimizationProblem:
+    """
+    Data class containing all necessary information of a minimization problem to support
+    steepest descent, newton, quasi-newton and conjugate minimization.
+
+    Args:
+        f (Callable): The function (objective) we are trying to minimize.
+        constraints (list): List of constraints functions for the minimization problem.
+        solution (list): The solution(s) to the minimization problem.
+                             Might contain multiple if there are multiple local minimizers.
+        x0 (list): The starting point for the minimization procedure.
+    """
+    f: Callable[[np.ndarray], np.ndarray]
+    constraints: np.ndarray # of Callables
+    solution: np.ndarray
+    x0: np.ndarray
+
+    # --- Objective function methods ---
+
+    def calc_f_at(self, x: np.ndarray) -> np.ndarray:
+        return self.f(x)
+
+    def calc_gradient_at(self, x: np.ndarray) -> np.ndarray:
+        return self._central_difference_gradient(self.f, x)
+
+    def calc_hessian_at(self,x: np.ndarray) -> np.ndarray:
+        return self.hessian_approximation(self.f, x)
+
+    # --- Constraint methods ---
+
+    def calc_constraint_at(self, i: int, x: np.ndarray) -> float: 
+        return self.constraints[i](x)
+
+    def calc_constraint_gradient_at(self, i: int, x: np.ndarray):
+        return self._central_difference_gradient(self.constraints[i], x)
+
+    # --- Lagrangian methods ---
+
+    def calc_lagrangian_at(self, x, l):
+        assert len(l) == len(self.constraints)
+
+        result = self.calc_f_at(x)
+
+        for i, l_i in enumerate(l):
+            result -= l_i * self.calc_constraint_at(i, x)
+        
+        return result
+    
+    # gradient wrt x
+    def calc_lagrangian_gradient_at(self, x, l):
+        def lagrangian(x):
+            return self.calc_lagrangian_at(x, l)
+
+        return self._central_difference_gradient(lagrangian, x)
+
+    # hessian wrt x
+    def calc_lagrangian_hessian_at(self, x, l):
+        def lagrangian(x):
+            return self.calc_lagrangian_at(x, l)
+
+        return self.hessian_approximation(lagrangian, x)
+
+    # --- Gradient/hessian approximation implementation ---
+
+    def _central_difference_gradient(self, f: Callable, x: np.ndarray) -> np.ndarray:
+        """Approximate gradient as described in equation (8.7), called the 'central difference formula'.
+
+        Args:
+            x (np.ndarray): Function input.
+
+        Returns:
+            np.ndarray: Approximated gradient.
+        """
+        eps = self._find_epsilon(x)
+        eps_vectors = np.eye(N=len(x)) * eps
+        return np.array([
+            (f(x + eps_vector) - f(x - eps_vector))/(2*eps) for eps_vector in eps_vectors
+        ])
+
+    def hessian_approximation(self, f: Callable, x: np.ndarray) -> np.ndarray:
+        """Approximate Hessian based on equation (8.21) in the book.
+
+        Args:
+            x (np.ndarray): Point for which we approximate the function's Hessian.
+
+        Returns:
+            np.ndarray: Approximated Hessian.
+        """
+        eps = self._find_epsilon(x)
+        eps_vectors = np.eye(N=len(x)) * eps
+        
+        hess = np.array([
+            [self._hess_approx_num(f, x, eps_i, eps_j) for eps_i in eps_vectors]
+            for eps_j in eps_vectors
+        ]) / (eps**2)
+        
+
+        # If the hessian approximation is basically 0, we are already close.
+        # Avoids SingularMatrix errors.
+        if sum(abs(x) for row in hess for x in row) < 0.0001: 
+            return np.eye(len(x))
+
+        return hess
+        
+    def _hess_approx_num(self, f: Callable, x: np.ndarray, eps_i: np.ndarray, eps_j: np.ndarray) -> float:
+        return f(x + eps_i + eps_j) - f(x + eps_i) - f(x + eps_j) + f(x)
+
+    def _find_epsilon(self, x: np.ndarray):
+        """Find computational error of the datatype of x and return it's square-root, as in equation (8.6).
+
+        Args:
+            x (np.ndarray): Array of which the datatype is considered.
+        """
+        try:
+            # Given the datatype of x, the below is the least number such that `1.0 + u != 1.0`.
+            u = np.finfo(x.dtype).eps
+
+        # x is an exact type, which throws an error; we use float64 instead, 
+        # as it is often the default when performing operations on ints which map to floats.
+        except (TypeError, ValueError): 
+            
+            u = np.finfo(np.float64).eps
+
+        epsilon = np.sqrt(u)
+
+        return epsilon
---
+++ sqp/problems.py
@@ -0,0 +1,25 @@
+import numpy as np
+from shared.classes import MinimizationProblem
+
+
+def create_example_18_3_problem():
+    """
+    Example 18.3 from the book
+    """
+    def f(x):
+        return np.e**(np.prod(x)) - 1/2*(x[0]**3 + x[1]**3 + 1)**2
+
+    def c1(x):
+        return np.sum(x**2) - 10
+    
+    def c2(x):
+        return x[1]*x[2] - 5*x[3]*x[4]
+    
+    def c3(x):
+        return x[0]**3 + x[1]**3 + 1
+    
+    x0 = np.array((-1.71, 1.59, 1.82, -0.763, -0.763))
+    solution = np.array((-1.8, 1.7, 1.9, -0.8, -0.8))
+    constraints = np.array((c1, c2, c3))
+
+    return MinimizationProblem(f=f, constraints=constraints, solution=solution, x0=x0)
