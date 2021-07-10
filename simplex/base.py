d138ced646392e3b9003f8cb039aaa08216ca39b
rendstrasser <rendstrasser@gmail.com>
2021-07-10 20:01:25 +0200

    first version of simplex with 3 problems

--- simplex/base.py
+++ simplex/base.py
@@ -8,18 +8,74 @@ def minimize_linear_problem(problem: LinearProblem) -> np.ndarray:
     A problem - assumed to be in standard form - is optimized.
     """
 
-    x = find_x0(problem)
     m = len(problem.b) # number of constraints -> size of basis
+    
+    # starting the simplex method
+    x0 = find_x0(problem)
 
     # the basis consists of x0 elements which are not 0. all others should be 0
-    x0_args_sorted = np.argsort(x)
-    basis = np.argsort(x)[-m:]
-    
-    # TODO implement algorithm 13.1
-    while True:
-        break
+    x0_args_sorted = np.argsort(x0)
+    basis = x0_args_sorted[-m:]
+    basis = np.sort(basis)
 
-    return x
+    non_basis = x0_args_sorted[:-m]
+    non_basis = np.sort(non_basis)
+    
+    # algorithm 13.1
+    i = 0
+    while True:
+        B = problem.A[:, basis]
+        N = problem.A[:, non_basis]
+
+        c_b = problem.c[basis]
+        c_n = problem.c[non_basis]
+
+        B_inv = np.linalg.inv(B)
+
+        if i == 0: # only necessary for initial run
+            x_b = B_inv @ problem.b
+
+        lambda_ = B_inv.T @ c_b
+        s_n = c_n - N.T @ lambda_
+
+        if np.all(s_n >= 0):
+            # optimal point found
+            x = np.zeros_like(x0)
+            x[basis] = x_b
+            return x
+
+        q = non_basis[np.argmin(s_n)]
+        A_q = problem.A[:, q].flatten()
+        d = B_inv @ A_q
+
+        if np.all(d <= 0):
+            raise ValueError("Problem is unbounded")
+
+        # find p
+        xb_div_d = x_b[d > 0]/d[d > 0]
+        min_div_idx = np.argmin(xb_div_d)
+        basis_idx = np.argwhere(d > 0)[min_div_idx]
+        p = basis[basis_idx]
+
+        # update basis/non-basis
+        remaining_basis_items_mask = basis != p
+        basis = basis[remaining_basis_items_mask]
+        basis = np.append(basis, q)
+        basis = np.sort(basis)
+        q_idx_in_basis = np.where(basis==q)[0]
+        
+        non_basis = non_basis[non_basis != q]
+        non_basis = np.append(non_basis, p)
+        non_basis = np.sort(non_basis)
+
+        # update x_b according to x_q^plus and x_b^plus in procedure 13.1
+        # to avoid a usage of an inverted B and matrix multiplication
+        x_q = xb_div_d[min_div_idx]
+        x_b -= d * x_q
+        x_b = x_b[remaining_basis_items_mask]
+        x_b = np.insert(x_b, q_idx_in_basis, x_q)
+
+        i += 1
 
 
 def find_x0(problem: LinearProblem):
@@ -33,6 +89,8 @@ def find_x0(problem: LinearProblem):
 
     x0 = xz0[:problem.n]
     z0 = xz0[problem.n:]
+
+    print(phase_I_problem.calc_f_at(xz0))
 
     if np.any(np.absolute(z0) > 1e-4):
         # no solution!
---
+++ simplex/problems.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+from simplex.linear_problem import LinearProblem, LinearCallable, LinearConstraint
+
+def create_example_13_1_problem():
+    c = np.array([-4, -2, 0, 0], dtype=np.float64)
+
+    a1 = np.array([1, 1, 1, 0], dtype=np.float64)
+    a2 = np.array([2, 1/2, 0, 1], dtype=np.float64)
+    constraints = np.array([
+        LinearConstraint(LinearCallable(a=a1, b=5), is_equality=True),
+        LinearConstraint(LinearCallable(a=a2, b=8), is_equality=True),
+    ])
+
+    #x0 = np.array([0, 0, 5, 8], dtype=np.float64)
+    x0 = None
+    solution = np.array([11/3, 4/3, 0, 0], dtype=np.float64)
+
+    return LinearProblem(c=c, n=4, constraints=constraints, x0=x0, solution=solution)
+
+def create_another_example_1():
+    """
+    Example 1 from https://realpython.com/linear-programming-python/
+    """ 
+
+    c = np.array([-1, -2], dtype=np.float64)
+
+    a1 = np.array([2, 1], dtype=np.float64)
+    a2 = np.array([-4, 5], dtype=np.float64)
+    a3 = np.array([1, -2], dtype=np.float64)
+    a4 = np.array([-1, 5], dtype=np.float64)
+    constraints = np.array([
+        LinearConstraint(LinearCallable(a=a1, b=20), is_equality=False),
+        LinearConstraint(LinearCallable(a=a2, b=10), is_equality=False),
+        LinearConstraint(LinearCallable(a=a3, b=2), is_equality=False),
+        LinearConstraint(LinearCallable(a=a4, b=15), is_equality=True),
+    ])
+
+    x0 = None
+    solution = np.array([85/11, 50/11], dtype=np.float64)
+
+    return LinearProblem(c=c, n=2, constraints=constraints, x0=x0, solution=solution)
+
+def create_another_example_2():
+    """
+    Example 2 from https://realpython.com/linear-programming-python/
+    """ 
+
+    c = np.array([-20, -12, -40, -25], dtype=np.float64)
+
+    a1 = np.array([1, 1, 1, 1], dtype=np.float64)
+    a2 = np.array([3, 2, 1, 0], dtype=np.float64)
+    a3 = np.array([0, 1, 2, 3], dtype=np.float64)
+    constraints = np.array([
+        LinearConstraint(LinearCallable(a=a1, b=50), is_equality=False),
+        LinearConstraint(LinearCallable(a=a2, b=100), is_equality=False),
+        LinearConstraint(LinearCallable(a=a3, b=90), is_equality=False)
+    ])
+
+    x0 = None
+    solution = np.array([5, 0, 45, 0], dtype=np.float64)
+
+    return LinearProblem(c=c, n=4, constraints=constraints, x0=x0, solution=solution)
