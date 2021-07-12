1c095f27be472b1d35518ed7ff5f81fbd99ea96a
franziska-denk <denk.m.franziska@gmail.com>
2021-07-12 10:16:17 +0200

    added: PLU factorization to avoid using np.linalg.inv(x) modified: base such that it uses the PLU-factorization modified: documentation in cholesky commented out minor detail in simplex.ipynb

--- shared/solve_linear_system.py
+++ shared/solve_linear_system.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def cholesky_factorization(A):
     """
-    factorizes input matrix in lower triangular matrix
+    factorizes input matrix in lower triangular matrix, requires the matrix to be symmetric and positive definite
     :param A: has to me a square matrix
     :return: lower triangular matrix
     """
@@ -24,6 +24,49 @@ def cholesky_factorization(A):
     return L
 
 
+def LU_factorization(A: np.ndarray):
+    """
+    factorizes a square matrix, matrix does not have to be symmetric or positive definite
+    use as A = LU
+    :param A: matrix to decompose
+    :return: lower matrix L and upper matrix U
+    """
+    assert A.shape[0] == A.shape[1]  # requires square matrix
+
+    n = A.shape[0]
+
+    # pivot matrix
+    p = np.eye(n)
+    for j in range(n):
+        max_row = j + np.argmax(np.abs(A[j:n, j]))
+        if j != max_row:  # swap rows if off-diagonal element is bigger in j-th column
+            p[[max_row, j]] = p[[j, max_row]]
+
+    L = np.zeros_like(A, dtype=np.float64)
+    U = np.zeros_like(A, dtype=np.float64)
+    PA = np.matmul(p, A)
+
+    for i in range(n):
+        # calculate U
+        for j in range(i,n):
+            s = 0
+            for k in range(i):
+                s += L[i, k] * U[k, j]
+            U[i, j] = PA[i, j] - s
+
+        # calculate L
+        for j in range(i, n):
+            if j == i:
+                L[i, j] = 1
+            else:
+                s = 0
+                for k in range(i):
+                    s += L[j, k] * U[k, i]
+                L[j, i] = (PA[j, i] - s) / U[i, i]
+
+    return L, U, p
+
+
 def forward(L, b):
     y = []
     for i in range(len(b)):
@@ -43,15 +86,24 @@ def backward(Lt, y):
     return x
 
 
-def solve(A, b):
+def solve(A, b, cholesky = False):
     """
-    solves Ax = b without computing the inverse of A but uses forward/backward substitution and cholesky factorization
+    solves Ax = b without computing the inverse of A but uses forward/backward substitution and the specified substitution
+    by default uses PLU factorization and hence can be used for arbitrary square matrices
+    (! attention: might fail if a column has only negative and 0 entries !, if needed @Franzi modifies it)
     :param A: square matrix
     :param b: solution to linear system
+    :param cholesky: if set to false, PLU factorization is used. use cholesky only for positive definite matrices, else LU
     :return: x (np.array with shape n x 1)
     """
-    L = cholesky_factorization(A)
-    y = forward(L, b)
-    solution = backward(L.T, y)
+    if cholesky:
+        L = cholesky_factorization(A)
+        y = forward(L, b)
+        solution = backward(L.T, y)
+
+    else:
+        L, U, P = LU_factorization(A)
+        y = forward(L, P @ b)
+        solution = backward(U, y)
 
     return solution
